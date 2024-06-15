2856f28380d2109b1c483930b76fa360da2924a9
Yiğit Bektaş Gürsoy <gursoy18@itu.edu.tr>
2024-06-15 22:01:38 +0300

    update

--- rtl/InstantMinComp.v
+++ rtl/InstantMinComp.v
@@ -1,51 +1,53 @@
 module InstantMinComp
-    #(parameter MAX_DATA_WIDTH = 16,
-                PE_COUNT = 16
-    )
-    (
-        input [MAX_DATA_WIDTH-1:0] in_SAD0, in_SAD1, in_SAD2, in_SAD3, in_SAD4, in_SAD5, in_SAD6, in_SAD7, in_SAD8, in_SAD9, in_SAD10, in_SAD11, in_SAD12, in_SAD13, in_SAD14, in_SAD15,
-        input [PE_COUNT-1:0] SAD_valid, // ONLY USING MSB FOR VALID SIGNAL
-        output out_SAD_valid_masked,
-        output reg [MAX_DATA_WIDTH-1:0] out_min_SAD
-    );
+	#(parameter MAX_DATA_WIDTH = 16,
+	            PE_COUNT = 16
+	)
+	
+	(
+    input [MAX_DATA_WIDTH-1:0] in_SAD0, in_SAD1, in_SAD2, in_SAD3, in_SAD4, in_SAD5, in_SAD6, in_SAD7,in_SAD8, in_SAD9, in_SAD10, in_SAD11, in_SAD12, in_SAD13, in_SAD14, in_SAD15,
+	input [PE_COUNT-1:0] SAD_valid, // ONLY USING MSB FOR VALID SIGNAL
+	output out_SAD_valid_masked,
+    output reg [MAX_DATA_WIDTH-1:0] out_min_SAD
+);
 
     reg [MAX_DATA_WIDTH-1:0] in_SAD[0:PE_COUNT-1];
-    wire SAD_valid_masking;
-    
-    assign SAD_valid_masking = (SAD_valid[15] == 1'b1) ? 1'b1 : 1'b0;
-    assign out_SAD_valid_masked = SAD_valid_masking;                            
-    
-    // 1D SAD ARRAY BEGIN 
+	wire SAD_valid_masking;
+	
+	
+	assign SAD_valid_masking = (SAD_valid[15] == 1'b1)? 1'b1: 1'b0;
+	assign out_SAD_valid_masked = SAD_valid_masking;							
+	
+	// 1D SAD ARRAY BEGIN 
     always @(*) begin // Populate the SAD array with input values
         in_SAD[0] = in_SAD0;
         in_SAD[1] = in_SAD1;
-        in_SAD[2] = in_SAD2;
+		in_SAD[2] = in_SAD2;
         in_SAD[3] = in_SAD3;
         in_SAD[4] = in_SAD4;
         in_SAD[5] = in_SAD5;
-        in_SAD[6] = in_SAD6;
+		in_SAD[6] = in_SAD6;
         in_SAD[7] = in_SAD7;
         in_SAD[8] = in_SAD8;
         in_SAD[9] = in_SAD9;
-        in_SAD[10] = in_SAD10;
+		in_SAD[10] = in_SAD10;
         in_SAD[11] = in_SAD11;
         in_SAD[12] = in_SAD12;
         in_SAD[13] = in_SAD13;
-        in_SAD[14] = in_SAD14;
+		in_SAD[14] = in_SAD14;
         in_SAD[15] = in_SAD15;
-    end
-    // 1D SAD ARRAY END
-    
-    // COMPARATOR ALGORITHM BEGIN
-    integer i;
+	end
+	// 1D SAD ARRAY END
+	
+	// COMPARATOR ALGORITHM BEGIN
+	integer i;
     always @(*) begin  // Initialize minimum SAD to the first value
         out_min_SAD = in_SAD[0]; 
-        if (SAD_valid_masking) begin // Only proceed if the overall valid signal is set
-            for (i = 1; i < 16; i = i + 1) begin
-                if (out_min_SAD > in_SAD[i]) begin
-                    out_min_SAD = in_SAD[i]; 
-                end
-            end
+		if(SAD_valid_masking) begin // Only proceed if the overall valid signal is set
+			for (i = 1; i < 16; i = i + 1) begin
+				if (out_min_SAD > in_SAD[i]) begin
+					out_min_SAD = in_SAD[i]; 
+				end
+			end
         end
     end
     // COMPARATOR ALGORITHM END
--- rtl/MinTrackerComp.v
+++ rtl/MinTrackerComp.v
@@ -1,54 +1,59 @@
 `timescale 1ns / 1ps
 
 module MinTrackerComp
-    #(parameter MAX_DATA_WIDTH = 16,
-      parameter COUNTER_WIDTH = 9
-    )    
-    (
-        input in_clk,
-        input in_rst,
-        input [MAX_DATA_WIDTH-1:0] in_min_SAD,
-        input in_SAD_valid_masked,
-        
-        output reg [MAX_DATA_WIDTH-1:0] out_final_min_SAD,
-        output reg out_DONE
+	#(parameter MAX_DATA_WIDTH = 16,
+	  parameter COUNTER_WIDTH = 9
+    )	
+
+	(
+	input in_clk,
+	input in_rst,
+	input [MAX_DATA_WIDTH-1:0] in_min_SAD,
+	input in_SAD_valid_masked,
+	
+	output reg [MAX_DATA_WIDTH-1:0] out_final_min_SAD,
+	output reg out_DONE
+
     );
-    
-    reg [MAX_DATA_WIDTH-1:0] reg_min_SAD; // Register to store the current minimum SAD
-    reg [COUNTER_WIDTH-1:0] SAD_valid_counter;
-    
-    always @(posedge in_clk or posedge in_rst) begin
-        if (in_rst) begin
-            reg_min_SAD <= 20'hFFFFF;   // Initialize with maximum possible value on reset
-        end else begin
-            // Process only when the SAD value is valid
-            // On the first valid input, store the SAD value in reg_min_SAD.
-            // On the second valid input, compare the stored value with the new input.
-            // If the new input is smaller, update reg_min_SAD. 
-            // Otherwise, the stored value in reg_min_SAD is the final minimum and output it.
-            if (in_SAD_valid_masked) begin 
-                if (reg_min_SAD >= in_min_SAD) begin
-                    reg_min_SAD <= in_min_SAD;
-                end
-            end
-        end
-    end
-    
-    // PROCESS COUNTER BEGIN
-    always @(posedge in_clk or posedge in_rst) begin
-        if (in_rst) begin
+	
+	reg [MAX_DATA_WIDTH-1:0] reg_min_SAD; // Register to store the current minimum SAD
+	reg [COUNTER_WIDTH-1:0] SAD_valid_counter;
+	
+	always @(posedge in_clk or posedge in_rst)begin
+		if(in_rst)begin
+			reg_min_SAD <= 20'hFFFFF;   // Initialize with maximum possible value on reset
+		end
+		else begin
+            /*Process only when the SAD value is valid
+            On the first valid input, store the SAD value in reg_min_SAD.
+            On the second valid input, compare the stored value with the new input.
+            If the new input is smaller, update reg_min_SAD. 
+            Otherwise, the stored value in reg_min_SAD is the final minimum and output it. */			
+ 			if(in_SAD_valid_masked)begin 
+				if(reg_min_SAD >= in_min_SAD)begin
+					reg_min_SAD <= in_min_SAD;
+				end
+			end
+		end
+	end
+	
+	//PROCESS COUNTER BEGIN
+	always @(posedge in_clk or posedge in_rst)begin
+	   if(in_rst)begin
             SAD_valid_counter <= 0;
             out_DONE <= 0;
             out_final_min_SAD <= 0;
-        end else begin
-            if (in_SAD_valid_masked) begin
+        end
+        else begin
+            if(in_SAD_valid_masked)begin
                 SAD_valid_counter <= SAD_valid_counter + 1'b1;
-            end else if (SAD_valid_counter == 16) begin
+            end
+            else if (SAD_valid_counter == 16)begin
                 out_DONE <= 1'b1;
                 out_final_min_SAD <= reg_min_SAD;
             end
         end
     end
-    // PROCESS COUNTER END
-    
+	//PROCESS COUNTER END
+	
 endmodule
